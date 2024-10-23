4e2a3e0952d6258b77ec182792aef9c5828e09ed
Ananthamurthy <ananthamurthy2004@gmail.com>
2024-10-23 17:13:20 +0530

    trying to redeploy

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-6=n4e9o!sxmys82a0+9uygy_72foa^wltdmj9d_0)+%9)bo!ft
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.railway.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
