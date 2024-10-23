b825c85a05e765d8d0af2469be53b2bd41881afe
Ananthamurthy <ananthamurthy2004@gmail.com>
2024-10-23 17:49:08 +0530

    static file commit

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -123,7 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Ensure this directory is created
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
